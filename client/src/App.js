cba31820aa806d369e8f38c59053a1b2a28da92e
Cameron White <whitcam20@gmail.com>
2022-01-11 23:19:25 -0500

    switching router to apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
+    <ApolloProvider client={client}>
       <>
         <Navbar />
         <Switch>
@@ -15,7 +21,7 @@ function App() {
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </Switch>
       </>
-    </Router>
+    </ApolloProvider>
   );
 }
 
